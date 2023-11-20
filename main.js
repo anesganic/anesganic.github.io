e19e26a810c841991cc015efc3ac39fe2074fda0
anesganic <108254745+anesganic@users.noreply.github.com>
2023-11-20 18:14:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const cursor = document.querySelector(".cursor");
 const links = document.querySelectorAll("a");
 const buttons = document.querySelectorAll(".mode-buttons button");
-const images = document.querySelectorAll('img'); // Get all images
+const images = document.querySelectorAll('img');
 
 document.addEventListener("mousemove", (e) => {
   let leftPosition = e.pageX + 4;
@@ -35,11 +35,11 @@ buttons.forEach(button => {
 
 images.forEach(image => {
   image.addEventListener('mouseenter', () => {
-    cursor.classList.add('large'); // Enlarge the cursor when hovering over an image
+    cursor.classList.add('large');
   });
 
   image.addEventListener('mouseleave', () => {
-    cursor.classList.remove('large'); // Revert the cursor size when leaving the image
+    cursor.classList.remove('large');
   });
 });
 
@@ -50,22 +50,22 @@ const body = document.body;
 const frame = document.querySelector('.frame');
 const h2Text = document.querySelector('h2');
 const projectsListItems = document.querySelectorAll('.projects_list li a');
-const apexImages = document.querySelectorAll('.apex_images img'); // Get all Apex images
+const apexImages = document.querySelectorAll('.apex_images img');
 
-// Function to toggle image inversion
+
 function toggleImageInversion() {
   const logoImage = document.getElementById('logo-image');
   if (body.classList.contains('dark-mode')) {
-    // Dark mode is active, remove the inverted-image class
+
     logoImage.classList.remove('inverted-image');
     apexImages.forEach(image => {
-      image.style.borderColor = 'white'; // Update the image border color
+      image.style.borderColor = 'white';
     });
   } else {
-    // Light mode is active, apply the inverted-image class
+
     logoImage.classList.add('inverted-image');
     apexImages.forEach(image => {
-      image.style.borderColor = 'black'; // Update the image border color
+      image.style.borderColor = 'black';
     });
   }
 }
@@ -83,7 +83,7 @@ lightModeButton.addEventListener('click', () => {
     item.style.color = '#070707';
   });
   localStorage.setItem('theme', 'light-mode');
-  toggleImageInversion(); // Toggle image inversion
+  toggleImageInversion();
   smoothTransition();
 });
 
@@ -97,10 +97,10 @@ darkModeButton.addEventListener('click', () => {
   darkModeButton.style.color = '#fff';
   h2Text.style.color = '#fff';
   projectsListItems.forEach(item => {
-    item.style.color = '#fff'; // Adjust as needed
+    item.style.color = '#fff';
   });
   localStorage.setItem('theme', 'dark-mode');
-  toggleImageInversion(); // Toggle image inversion
+  toggleImageInversion();
   smoothTransition();
 });
 
@@ -114,7 +114,7 @@ if (savedTheme) {
   darkModeButton.style.color = savedTheme === 'light-mode' ? '#070707' : '#fff';
   h2Text.style.color = savedTheme === 'light-mode' ? '#070707' : '#fff';
   projectsListItems.forEach(item => {
-    item.style.color = savedTheme === 'light-mode' ? '#070707' : '#fff'; // Adjust as needed
+    item.style.color = savedTheme === 'light-mode' ? '#070707' : '#fff';
   });
-  toggleImageInversion(); // Set the logo color on page load
+  toggleImageInversion();
 }
